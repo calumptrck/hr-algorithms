91d0f30f0aaa4c36681920bb5452a20074821949
calumptrck <strummed11@gmail.com>
2017-02-14 20:36:33 -0500

    Angry Professor

---
+++ Angry Professor.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n,k = input().strip().split(' ')
+    n,k = [int(n),int(k)]
+    a = [int(a_temp) for a_temp in input().strip().split(' ')]
+    attend = len([x for x in a if x<=0])
+    if attend >= k:
+        print('NO')
+    else:
+        print('YES')
